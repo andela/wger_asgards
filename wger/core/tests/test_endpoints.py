7697d5b375480cc22940ce038ae320d76a2ffbbd
izlyr <fuzzkillofficial@gmail.com>
2017-01-23 10:59:34 +0300

    [Chore]: Write test endpoints for adding and viewing users-137085745

---
+++ wger/core/tests/test_endpoints.py
@@ -0,0 +1,23 @@
+import json
+from django.urls import reverse
+from rest_framework import status
+from wger.core.tests.base_testcase import WorkoutManagerTestCase
+from django.contrib.auth.models import User
+
+
+class UserEndpointTests(WorkoutManagerTestCase):
+    def test_adds_user(self):
+        """
+Ensure we can create a new User object
+        """
+        url = reverse('core:user:add')
+        data = {'name': 'Boniface'}
+        response = self.client.post(url, data, formart='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.get().name, 'Bomiface')
+
+    def view_users(self):
+        response = self.client.get('/user/1/')
+        self.assertEqual(json.loads(response.content), {
+                         'id': 1, 'username': "boniface"})
